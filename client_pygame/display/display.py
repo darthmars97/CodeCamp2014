46d87e9d2a0e82c23eb8a5e136cac00d11cfe53b
Marshall Schmutz <darthmars97@users.noreply.github.com>
2014-11-21 11:56:15 -0600

    New Waiting Message
    I changed the waiting message to be a bit more aggressive.

--- client_pygame/display/display.py
+++ client_pygame/display/display.py
@@ -133,7 +133,7 @@ class Display(BaseDisplay):
         rect = pygame.Rect(0, 0, self.width, self.height)
         surface.fill(self.background_color, rect)
         # text message in center of screen
-        s = "Waiting for opponent to connect."
+        s = "Waiting for an opponent who is willing to get destroyed to connect."
         self.draw_text_center(surface, s, self.text_color,
                               self.width/2, self.height/2,
                               self.font)
