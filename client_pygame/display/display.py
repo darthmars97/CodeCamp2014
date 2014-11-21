067fa6cb9c41eca1293ed00dc967c9e57f279c99
Marshall Schmutz <darthmars97@users.noreply.github.com>
2014-11-21 16:19:52 -0600

    Player Up Animation
    Fixed the images for this animation.  Now it works!

--- client_pygame/display/display.py
+++ client_pygame/display/display.py
@@ -100,7 +100,7 @@ class Display(BaseDisplay):
         self.player_image_front1 = pygame.image.load("FrontWalk1.png")
         self.player_image_front2 = pygame.image.load("FrontWalk2.png")
         self.player_image_back1 = pygame.image.load("BackWalk1.png")
-        self.player_image_back1 = pygame.image.load("BackWalk2.png")
+        self.player_image_back2 = pygame.image.load("BackWalk2.png")
         self.image_count      = 0
         self.opponent_color   = (255, 0, 0)
         self.missile_color    = (0, 255, 255)
@@ -247,14 +247,14 @@ class Display(BaseDisplay):
                     else:
                         # facing up image
                         if self.image_count <= 5:
-                            surface.blit(self.player_image_front1, (obj.get_px(), obj.get_py()))
+                            surface.blit(self.player_image_back1, (obj.get_px(), obj.get_py()))
                             self.image_count += 1
                         elif 5 < self.image_count <= 10:
-                            surface.blit(self.player_image_front2, (obj.get_px(), obj.get_py()))
+                            surface.blit(self.player_image_back2, (obj.get_px(), obj.get_py()))
                             self.image_count += 1
                         elif self.image_count > 10:
                             self.image_count = 0
-                            surface.blit(self.player_image_front1, (obj.get_px(), obj.get_py()))
+                            surface.blit(self.player_image_back1, (obj.get_px(), obj.get_py()))
                 elif obj.get_dx > 0:
                     if abs(obj.get_dx()) > abs(obj.get_dy()):
                         # facing left image
