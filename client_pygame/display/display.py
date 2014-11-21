a4936b0148b035509b7beb70bcf82cccad3bfac6
Marshall Schmutz <darthmars97@users.noreply.github.com>
2014-11-21 15:10:11 -0600

    Player Down Animation
    Player is animated while moving down.  It works somewhat, lagging.
    Needs fine tuning.

--- client_pygame/display/display.py
+++ client_pygame/display/display.py
@@ -97,6 +97,9 @@ class Display(BaseDisplay):
         # Check out http://www.colorpicker.com/ if you want to try out
         # colors and find their RGB values.
         self.player_image     = pygame.image.load("FrontStandard.png")
+        self.player_image_front1 = pygame.image.load("FrontWalk1.png")
+        self.player_image_front2 = pygame.image.load("FrontWalk2.png")
+        self.image_count      = 0
         self.opponent_color   = (255, 0, 0)
         self.missile_color    = (0, 255, 255)
         self.npc_color        = (255, 255, 0)
@@ -235,7 +238,29 @@ class Display(BaseDisplay):
             if obj.get_oid() == engine.get_player_oid():
                 #color = self.player_color
                 # Marshall the next line draws a image for the player
-                surface.blit(self.player_image, (obj.get_px(), obj.get_py()))
+                if obj.get_dx() <= 0:
+                    if abs(obj.get_dx()) > abs(obj.get_dy()):
+                        # facing right image
+                        surface.blit(self.player_image_front1, (obj.get_px(), obj.get_py()))
+                    else:
+                        # facing up image
+                        surface.blit(self.player_image_front1, (obj.get_px(), obj.get_py()))
+                elif obj.get_dx > 0:
+                    if abs(obj.get_dx()) > abs(obj.get_dy()):
+                        # facing left image
+                        surface.blit(self.player_image_front2, (obj.get_px(), obj.get_py()))
+                    else:
+                        # facing down image
+                        if self.image_count <= 5:
+                            surface.blit(self.player_image_front1, (obj.get_px(), obj.get_py()))
+                            self.image_count += 1
+                        elif 5 < self.image_count <= 10:
+                            surface.blit(self.player_image_front2, (obj.get_px(), obj.get_py()))
+                        elif self.image_count > 10:
+                            self.image_count = 0
+                            surface.blit(self.player_image_front2, (obj.get_px(), obj.get_py()))
+                else:
+                    surface.blit(self.player_image, (obj.get_px(), obj.get_py()))
             else:
                 color = self.opponent_color
                 pygame.draw.rect(surface, color, rect)
