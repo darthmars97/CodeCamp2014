b44a968419177339f8423870e47bdeae9e11a690
Marshall Schmutz <darthmars97@users.noreply.github.com>
2014-11-22 00:53:36 -0600

    Enemy Missile
    Enemy missiles are fireballs

--- client_pygame/display/display.py
+++ client_pygame/display/display.py
@@ -126,6 +126,10 @@ class Display(BaseDisplay):
         self.arrow_image_down = pygame.image.load("ArrowDown.png")
         self.arrow_image_left = pygame.image.load("ArrowLeft.png")
         self.arrow_image_right = pygame.image.load("ArrowRight.png")
+        self.fireball_up = pygame.image.load("FireballUp.png")
+        self.fireball_down = pygame.image.load("FireballDown.png")
+        self.fireball_left = pygame.image.load("FireballLeft.png")
+        self.fireball_right = pygame.image.load("FireballRight.png")
         self.npc_image1       = pygame.image.load("NPC1.png")
         self.npc_image2       = pygame.image.load("NPC2.png")
         self.npc_image_count  = 0
@@ -259,21 +263,38 @@ class Display(BaseDisplay):
         Draws living missiles.
         """
         if obj.is_alive():
-            rect = self.obj_to_rect(obj)
-            if obj.get_dx() <= 0:
-                    if abs(obj.get_dx()) > abs(obj.get_dy()):
-                        # facing left image
-                        surface.blit(self.arrow_image_left, rect)
-                    else:
-                        # facing up image
-                        surface.blit(self.arrow_image_up, rect)
-            elif obj.get_dx > 0:
-                    if abs(obj.get_dx()) > abs(obj.get_dy()):
-                        # facing right image
-                        surface.blit(self.arrow_image_right, rect)
-                    else:
-                        # facing down image
-                        surface.blit(self.arrow_image_down, rect)
+            if obj.get_player_oid() == engine.get_player_oid():
+                rect = self.obj_to_rect(obj)
+                if obj.get_dx() <= 0:
+                        if abs(obj.get_dx()) > abs(obj.get_dy()):
+                            # facing left image
+                            surface.blit(self.arrow_image_left, rect)
+                        else:
+                            # facing up image
+                            surface.blit(self.arrow_image_up, rect)
+                elif obj.get_dx > 0:
+                        if abs(obj.get_dx()) > abs(obj.get_dy()):
+                            # facing right image
+                            surface.blit(self.arrow_image_right, rect)
+                        else:
+                            # facing down image
+                            surface.blit(self.arrow_image_down, rect)
+            else:
+                rect = self.obj_to_rect(obj)
+                if obj.get_dx() <= 0:
+                        if abs(obj.get_dx()) > abs(obj.get_dy()):
+                            # facing left image
+                            surface.blit(self.fireball_left, rect)
+                        else:
+                            # facing up image
+                            surface.blit(self.fireball_up, rect)
+                elif obj.get_dx > 0:
+                        if abs(obj.get_dx()) > abs(obj.get_dy()):
+                            # facing right image
+                            surface.blit(self.fireball_right, rect)
+                        else:
+                            # facing down image
+                            surface.blit(self.fireball_down, rect)
         return
         
     def paint_player(self, surface, engine, control, obj):
